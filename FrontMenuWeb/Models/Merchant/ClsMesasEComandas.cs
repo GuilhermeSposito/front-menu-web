792ba5eeb1f52c4eed418eaf3b5dc39c50b343c5
Guilherme Sposito <guilhermesposito14@gmail.com>
2025-10-01 16:55:10 -0300

    adicionando corr

--- FrontMenuWeb/Models/Merchant/ClsMesasEComandas.cs
+++ FrontMenuWeb/Models/Merchant/ClsMesasEComandas.cs
@@ -5,7 +5,7 @@ namespace FrontMenuWeb.Models.Merchant;
 public class ClsMesasEComandas
 {
     [JsonPropertyName("id")] public int Id { get; set; }
-    [JsonPropertyName("CodigoExterno")] public int CodigoExterno { get; set; }
+    [JsonPropertyName("CodigoExterno")] public int CodigoExterno { get; set; } = 1;
     [JsonPropertyName("Bloqueado")] public bool Bloqueado { get; set; }
     [JsonPropertyName("Reservado")] public bool Reservado { get; set; }
     [JsonPropertyName("InicioDoIntervalo")][JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] public int? InicioDoIntervalo { get; set; } = 0;
