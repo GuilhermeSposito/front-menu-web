7d3cfdf0da40ea6f2229c206eade192ff2734a1f
Guilherme Sposito <guilhermesposito14@gmail.com>
2025-12-08 11:00:04 -0300

    Adicionando cerebro na  barra

--- FrontDesktopBlzHybrid/App.xaml.cs
+++ FrontDesktopBlzHybrid/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace FrontDesktopBlzHybrid
+﻿using Microsoft.Maui.Platform;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+
+namespace FrontDesktopBlzHybrid
 {
     public partial class App : Application
     {
@@ -6,10 +10,47 @@
         {
             InitializeComponent();
         }
-
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "Sophos Desktop" };
+            var window = new Window(new MainPage()) { Title = "Sophos Desktop" };
+            window.Created += (s, e) =>
+            {
+
+                SetFullScreen(window);
+            };
+
+            return window;
         }
+
+        public void SetFullScreen(Window mauiWindow)
+        {
+#if WINDOWS
+            try
+            {
+                // Obter a janela nativa
+                var nativeWindow = mauiWindow.Handler.PlatformView as Microsoft.UI.Xaml.Window;
+                if (nativeWindow == null)
+                    return;
+
+                // Obter AppWindow
+                var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(
+                    WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow)
+                );
+
+                var appWindow = AppWindow.GetFromWindowId(windowId);
+                if (appWindow.Presenter is OverlappedPresenter)
+                {
+                    appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Erro ao entrar em fullscreen: " + ex);
+            }
+#endif
+        }
+
     }
+
+
 }
