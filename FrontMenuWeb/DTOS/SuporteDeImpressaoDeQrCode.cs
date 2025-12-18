f6c22969635846caccefba5f38a63d6e0ef4c9d1
Guilherme Sposito <guilhermesposito14@gmail.com>
2025-12-18 10:06:20 -0300

    Adicionando Emissão de qrcode com descrição de qual mesa é

---
+++ FrontMenuWeb/DTOS/SuporteDeImpressaoDeQrCode.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FrontMenuWeb.DTOS;
+
+public class SuporteDeImpressaoDeQrCode
+{
+    public int IdMesa { get; set; }
+    public string CodigoExternoMesa { get; set; } = string.Empty;
+    public MarkupString QrCodeSvg { get; set; }
+}
