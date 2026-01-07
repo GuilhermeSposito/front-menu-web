3f26a4067bb871e92845487aaceee2f90c3823a7
Guilherme Sposito <guilhermesposito14@gmail.com>
2026-01-07 11:27:50 -0300

    Enviando o websoket sem o token na req

--- FrontMenuWeb/wwwroot/wsClient.js
+++ FrontMenuWeb/wwwroot/wsClient.js
@@ -9,10 +9,7 @@ window.socketIO = {
         const socket = io("https://syslogicadev.com", {
             path: "/socket.io/",
             transports: ["websocket"],
-            query:
-            {
-                token: `${token}`
-            }
+            withCredentials: true
         });
 
         socket.on("connect", () => {
