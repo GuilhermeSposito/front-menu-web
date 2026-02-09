dfc634b87b9e60c5d96c15d2c5e82e3393fdb2c6
Guilherme Sposito <guilhermesposito14@gmail.com>
2026-02-09 15:47:40 -0300

    Testatndo a conversão do ibpt

--- ApiFiscalMenuWeb/Services/NfService.cs
+++ ApiFiscalMenuWeb/Services/NfService.cs
@@ -927,6 +927,8 @@ public class NfService
             var valorTotalTrib = await _ibptServices.GetIBPTValor
                         (cnpj: CnpjMerchantAtual, ncm: item.Produto.NCM, uf: "SP", descricao: item.Produto.Descricao, item.PrecoTotal);
 
+            valorTotalTrib = (float)Math.Round(valorTotalTrib, 2, MidpointRounding.AwayFromZero);
+
             Console.WriteLine($"Valor total do tributo: {valorTotalTrib}");
 
             ValorTotalTribNfAtual += valorTotalTrib;
