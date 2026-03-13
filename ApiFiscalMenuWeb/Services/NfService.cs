31ac0dddc1ff8e8d4f57e7468ede97aa21f5eb5c
Guilherme Sposito <guilhermesposito14@gmail.com>
2026-03-13 00:28:37 -0300

    Corrigindo erro de calculo de nf

--- ApiFiscalMenuWeb/Services/NfService.cs
+++ ApiFiscalMenuWeb/Services/NfService.cs
@@ -790,6 +790,9 @@ public class NfService
             enNfCeDto.Pedido.TaxaEntregaValor = 0;
         }
 
+        var json = JsonSerializer.Serialize(enNfCeDto.Pedido.Itens);
+        Console.WriteLine(json);
+
         var detDosProd = await RetornaDetsDosProdutosNoPedido(ItensDoPedido: enNfCeDto.Pedido.Itens, Pedido: enNfCeDto.Pedido, TipoDFe.NFCe, tipoAmbiente);
 
         var xml = new EnviNFe
