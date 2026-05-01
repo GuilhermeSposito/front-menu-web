fa3f126abe91b38681845a5ee763b93d5607fe1d
Guilherme Sposito <guilhermesposito14@gmail.com>
2026-04-30 22:27:12 -0300

    adicionando correção de fechamento parcial42

--- FrontMenuWeb/Services/PedidosService.cs
+++ FrontMenuWeb/Services/PedidosService.cs
@@ -376,9 +376,9 @@ public class PedidosService
         return retorno!;
     }
 
-    public async Task<ReturnApiRefatored<ClsPedido>> AvisarFechamentoParcial(int mesaId, List<int> itemIds, float servicoValor = 0, float couvertValor = 0)
+    public async Task<ReturnApiRefatored<ClsPedido>> AvisarFechamentoParcial(int mesaId, List<int> itemIds, bool cobraTaxaServico, bool cobraCouvert, int qtdPessoas = 0)
     {
-        var body = new { mesaId, itemIds, ServicoValor = servicoValor, CouvertValor = couvertValor };
+        var body = new { mesaId, itemIds, cobraTaxaServico, cobraCouvert, qtdPessoas };
         var response = await _http.PostAsJsonAsync("pedidos/aviso-parcial", body);
         var retorno = await response.Content.ReadFromJsonAsync<ReturnApiRefatored<ClsPedido>>();
         return retorno!;
